83d4e74a1c71f6cdba7ffa97ee6c11f44964bab5
ViperPsychoIT <110948937+ViperPsychoIT@users.noreply.github.com>
2023-09-14 19:56:19 +0200

    Update download_models.py

--- src/download_models.py
+++ src/download_models.py
@@ -5,8 +5,8 @@ MDX_DOWNLOAD_LINK = 'https://github.com/TRvlvr/model_repo/releases/download/all_
 RVC_DOWNLOAD_LINK = 'https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/'
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-mdxnet_models_dir = BASE_DIR / 'mdxnet_models'
-rvc_models_dir = BASE_DIR / 'rvc_models'
+mdxnet_models_dir = BASE_DIR / '/content/drive/MyDrive/mdxnet_models'
+rvc_models_dir = BASE_DIR / '/content/drive/MyDrive/rvc_models'
 
 
 def dl_model(link, model_name, dir_name):
@@ -20,12 +20,12 @@ def dl_model(link, model_name, dir_name):
 if __name__ == '__main__':
     mdx_model_names = ['UVR-MDX-NET-Voc_FT.onnx', 'UVR_MDXNET_KARA_2.onnx', 'Reverb_HQ_By_FoxJoy.onnx']
     for model in mdx_model_names:
-        print(f'Downloading {model}...')
+        print(f'Download del modello {model}...')
         dl_model(MDX_DOWNLOAD_LINK, model, mdxnet_models_dir)
 
     rvc_model_names = ['hubert_base.pt', 'rmvpe.pt']
     for model in rvc_model_names:
-        print(f'Downloading {model}...')
+        print(f'Download del modello {model}...')
         dl_model(RVC_DOWNLOAD_LINK, model, rvc_models_dir)
 
-    print('All models downloaded!')
+    print('Tutti i modelli sono stati scaricati!')
